a0cc5d0f39d32d757f45aa8e60367e34e06940d2
Alex <tverdovskyi92@gmail.com>
2021-07-26 18:14:54 +0300

    changes

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '21816911-1420e6eb818d750af174e21f8';
 
-const findImage = (searchQuery, currentPage) =>
+const findImage = (currentPage, searchQuery) =>
   axios
     .get(
-      `?q=${searchQuery}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+      `?q=${searchQuery}&key=${API_KEY}&image_type=photo&orientation=horizontal&page=${currentPage}&per_page=12`,
     )
     .then(response => response.data.hits);
 
